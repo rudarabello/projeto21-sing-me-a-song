96646506eb02d75af3d7ac28a349c30f850b7355
ruda <ruda.rabello.silva@gmail.com>
2022-09-25 19:18:58 -0300

    feat: add  insert and downvote unit test

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -8,11 +8,29 @@ describe("Recommendations service test", () => {
         jest.clearAllMocks();
         jest.resetAllMocks();
     });
-
     it("should not found recommendation upvote", async () => {
         jest.spyOn(recommendationRepository, "find").mockResolvedValue(null);
         expect(async () => {
             await recommendationService.upvote(1);
         }).rejects.toEqual({ message: "", type: "not_found" });
     });
+    it("should not found recommendation downvote", async () => {
+        jest.spyOn(recommendationRepository, "find").mockResolvedValue(null);
+        expect(async () => {
+            await recommendationService.downvote(1);
+        }).rejects.toEqual({ message: "", type: "not_found" });
+    });
+    it("should conflict recommendation insert", async () => {
+        const recommendation = recommendationsAmount();
+        jest.spyOn(recommendationRepository, "findByName")
+            .mockResolvedValue(recommendation[0]);
+        expect(async () => {
+            await recommendationService.insert(recommendation[0]);
+        }).rejects.toEqual({
+            message: "Recommendations names must be unique",
+            type: "conflict",
+        });
+    });
+
+
 })
