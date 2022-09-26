789359a6434b08a1f775fe1e0ee703e4ed802faa
ruda <ruda.rabello.silva@gmail.com>
2022-09-26 17:48:13 -0300

    feat: add random test

--- front-end/cypress/e2e/tests/add-recommendation.cy.js
+++ front-end/cypress/e2e/tests/add-recommendation.cy.js
@@ -1,22 +1,22 @@
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
 
 beforeEach(() => {
-    cy.resetDatabase();
-});
+  cy.resetDatabase()
+})
 
 describe('Test post on recomendations', () => {
-    const recommendation = {
-        name: faker.name.firstName(),
-        link: faker.internet.url(),
-    };
+  const recommendation = {
+    name: faker.name.firstName(),
+    link: faker.internet.url()
+  }
 
-    it('add name and link recommendation', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('#name').type(recommendation.name);
-        cy.get('#link').type('https://www.youtube.com/watch?v=zZlIy3hp0c0&list=PLa75BYTPDNKbhUVggmU3JUEBPibvh0C2t&index=5');
-        cy.intercept('POST', 'http://localhost:5000/recommendations').as('newRecommendation');
-        cy.get('#submit').click();
-        cy.wait('@newRecommendation');
-        cy.contains(recommendation.name).should('be.visible');
-    });
-});
+  it('add name and link recommendation', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#name').type(recommendation.name)
+    cy.get('#link').type('https://www.youtube.com/watch?v=zZlIy3hp0c0&list=PLa75BYTPDNKbhUVggmU3JUEBPibvh0C2t&index=5')
+    cy.intercept('POST', 'http://localhost:5000/recommendations').as('newRecommendation')
+    cy.get('#submit').click()
+    cy.wait('@newRecommendation')
+    cy.contains(recommendation.name).should('be.visible')
+  })
+})
--- front-end/cypress/e2e/tests/down-vote-rec.cy.js
+++ front-end/cypress/e2e/tests/down-vote-rec.cy.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-undef */
 beforeEach(() => {
-    cy.resetDatabase();
-});
+  cy.resetDatabase()
+})
 
 describe('downvote a recommendation', () => {
-    it('downvote successfully', () => {
-        cy.visit('http://localhost:3000');
+  it('downvote successfully', () => {
+    cy.visit('http://localhost:3000')
 
-        cy.addRecommendation();
+    cy.addRecommendation()
 
-        cy.intercept('GET', '/recommendations').as('getRecommendations');
-        cy.wait('@getRecommendations');
+    cy.intercept('GET', '/recommendations').as('getRecommendations')
+    cy.wait('@getRecommendations')
 
-        cy.get('[data-cy=downvote]').click();
+    cy.get('[data-cy=downvote]').click()
 
-        cy.get('[data-cy=score]').should(($p) => {
-            expect($p).to.contain('-1');
-        });
-    });
-});
+    cy.get('[data-cy=score]').should(($p) => {
+      expect($p).to.contain('-1')
+    })
+  })
+})
